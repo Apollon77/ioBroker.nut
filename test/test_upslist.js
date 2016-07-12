c5d1bcd9b158ea9cbe13af483cba64084a375716
Ingo Fischer <github@fischer-ka.de>
2016-07-12 14:30:44 +0200

    fix test skript

--- test/test_upslist.js
+++ test/test_upslist.js
@@ -1,7 +1,12 @@
+if (process.argv.length<3) {
+  console.log('Call: test_upsvars.js <ip> <port> <ups-name>');
+  process.exit();
+}
+
 var Nut = require('node-nut');
 
 //oNut = new Nut(3493, 'localhost');
-oNut = new Nut(3493, '192.168.178.23');
+oNut = new Nut(process.argv[3], process.argv[2]);
 
 oNut.on('error', function(err) {
   console.log('There was an error: ' + err);
