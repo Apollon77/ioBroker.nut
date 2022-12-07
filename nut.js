796a827ab03442eb7e54d4731dfc3f00174084a6
Ingo Fischer <github@fischer-ka.de>
2022-12-07 22:58:45 +0100

    * updates * delay initialization when USV is not available

--- nut.js
+++ nut.js
@@ -11,15 +11,15 @@
  /*jslint esversion: 6 */
 'use strict';
 
-var utils = require('@iobroker/adapter-core'); // Get common adapter utils
-var adapter;
+const utils = require('@iobroker/adapter-core'); // Get common adapter utils
+let adapter;
 
-var Nut   = require('node-nut');
+const Nut = require('node-nut');
 
-var nutTimeout;
-var stopInProgress;
+let nutTimeout;
+let stopInProgress;
 
-var nutCommands = null;
+let nutCommands = null;
 let connected = null;
 
 function startAdapter(options) {
@@ -30,40 +30,44 @@ function startAdapter(options) {
     adapter = new utils.Adapter(options);
 
 
-    adapter.on('ready', function () {
+    adapter.on('ready', () => {
         main();
     });
 
-    adapter.on('message', function (msg) {
+    adapter.on('message', (msg) => {
         processMessage(msg);
     });
 
-    adapter.on('stateChange', function (id, state) {
+    adapter.on('stateChange', (id, state) => {
         adapter.log.debug('stateChange ' + id + ' ' + JSON.stringify(state));
-        var realNamespace = adapter.namespace + '.commands.';
-        var stateId = id.substring(realNamespace.length);
+        const realNamespace = adapter.namespace + '.commands.';
+        const stateId = id.substring(realNamespace.length);
         if (!state || state.ack || id.indexOf(realNamespace) !== 0) return;
 
-        var command = stateId.replace(/-/g,'.');
-        initNutConnection(function(oNut) {
+        const command = stateId.replace(/-/g, '.');
+        initNutConnection((oNut) => {
+            if (!oNut) {
+                adapter.log.error('USV not available - Error while sending command: ' + command);
+                return;
+            }
             if (stopInProgress) return; // adapter already unloaded
             if (adapter.config.username && adapter.config.password) {
                 adapter.log.info('send username for command ' + command);
-                oNut.SetUsername(adapter.config.username, function (err) {
+                oNut.SetUsername(adapter.config.username, (err) => {
                     if (err) {
                         adapter.log.error('Err while sending username: '+ err);
                         oNut.close();
                     }
                     else {
                         adapter.log.info('send password for command ' + command);
-                        oNut.SetPassword(adapter.config.password, function (err) {
+                        oNut.SetPassword(adapter.config.password,  (err) => {
                             if (err) {
                                 adapter.log.error('Err while sending password: '+ err);
                                 oNut.close();
                             }
                             else {
                                 adapter.log.info('send command ' + command);
-                                oNut.RunUPSCommand(adapter.config.ups_name, command, function (err) {
+                                oNut.RunUPSCommand(adapter.config.ups_name, command,  (err) => {
                                     if (err) {
                                         adapter.log.error('Err while sending command ' + command + ': '+ err);
                                         oNut.close();
@@ -77,7 +81,7 @@ function startAdapter(options) {
             }
             else {
                 adapter.log.info('send command ' + command + ' without username and password');
-                oNut.RunUPSCommand(adapter.config.ups_name, command, function (err) {
+                oNut.RunUPSCommand(adapter.config.ups_name, command,  (err) => {
                     if (err) {
                         adapter.log.error('Err while sending command ' + command + ': '+ err);
                     }
@@ -89,7 +93,7 @@ function startAdapter(options) {
         });
     });
 
-    adapter.on('unload', function (callback) {
+    adapter.on('unload', (callback) => {
         stopInProgress = true;
         if (nutTimeout) clearTimeout(nutTimeout);
         nutTimeout = null;
@@ -102,17 +106,6 @@ function startAdapter(options) {
 
     return adapter;
 }
-
-process.on('SIGINT', function () {
-    if (nutTimeout) clearTimeout(nutTimeout);
-});
-
-process.on('uncaughtException', function (err) {
-    if (adapter && adapter.log) {
-        adapter.log.warn('Exception: ' + err);
-    }
-    if (nutTimeout) clearTimeout(nutTimeout);
-});
 
 function setConnected(isConnected) {
     if (connected !== isConnected) {
@@ -131,7 +124,7 @@ function setConnected(isConnected) {
 
 async function main() {
     setConnected(false);
-    adapter.getForeignObject('system.adapter.' + adapter.namespace, function (err, obj) {
+    adapter.getForeignObject('system.adapter.' + adapter.namespace, (err, obj) => {
        if (!err && obj && (obj.common.mode !== 'daemon')) {
             obj.common.mode = 'daemon';
             if (obj.common.schedule) delete(obj.common.schedule);
@@ -173,33 +166,48 @@ async function main() {
             common: {
                 name: 'status.last_notify',
                 type: 'string',
+                role: 'state',
                 read: true,
                 write: false
             },
-            native: {id: 'status.last_notify'}
+            native: {
+                id: 'status.last_notify'
+            }
         });
     } catch (err) {
         adapter.log.error('Error creating State: ' + err);
     }
-    adapter.getState('status.last_notify', async function (err, state) {
+    adapter.getState('status.last_notify', async  (err, state) => {
         if (!err && !state) {
             await adapter.setStateAsync('status.last_notify', {ack: true, val: ''});
         }
-        initNutConnection(function(oNut) {
-            oNut.GetUPSCommands(adapter.config.ups_name, function(cmdlist, err) {
-                if (err) {
-                    adapter.log.error('Err while getting all commands: '+ err);
-                }
-                else {
-                    adapter.log.debug('Got commands, create and subscribe command states');
-                    initNutCommands(cmdlist);
-                }
+        initializeNutDevice();
+    });
+}
 
-                getCurrentNutValues(oNut, true);
+function initializeNutDevice() {
+    const update_interval = parseInt(adapter.config.update_interval,10) || 60;
 
-                var update_interval = parseInt(adapter.config.update_interval,10) || 60;
-                nutTimeout = setTimeout(updateNutData, update_interval * 1000);
-            });
+    initNutConnection((oNut) => {
+        if (!oNut) {
+            adapter.log.error('USV not available - Delay initialization');
+
+            nutTimeout = setTimeout(initializeNutDevice, update_interval * 1000);
+
+            return;
+        }
+        oNut.GetUPSCommands(adapter.config.ups_name, (cmdlist, err) => {
+            if (err) {
+                adapter.log.error('Err while getting all commands: '+ err);
+            }
+            else {
+                adapter.log.debug('Got commands, create and subscribe command states');
+                initNutCommands(cmdlist);
+            }
+
+            getCurrentNutValues(oNut, true);
+
+            nutTimeout = setTimeout(updateNutData, update_interval * 1000);
         });
     });
 }
@@ -218,8 +226,8 @@ async function initNutCommands(cmdlist) {
 
     if (! cmdlist) return;
     nutCommands = cmdlist;
-    for (var i = 0; i < cmdlist.length; i++) {
-        var cmdName = cmdlist[i].replace(/\./g,'-');
+    for (let i = 0; i < cmdlist.length; i++) {
+        const cmdName = cmdlist[i].replace(/\./g, '-');
         adapter.log.debug('Create State commands.' + cmdName);
         try {
             await adapter.setObjectNotExistsAsync('commands.' + cmdName, {
@@ -259,15 +267,17 @@ function processMessage(message) {
 
     adapter.log.info('Message received = ' + JSON.stringify(message));
 
-    var updateNut = false;
+    let updateNut = false;
     if (message.command === 'notify' && message.message) {
         adapter.log.info('got Notify ' + message.message.notifytype + ' for: ' + message.message.upsname);
-        var ownName = adapter.config.ups_name + '@' + adapter.config.host_ip;
+        const ownName = adapter.config.ups_name + '@' + adapter.config.host_ip;
         adapter.log.info('ownName=' + ownName + ' --> ' + (ownName === message.message.upsname));
         if (ownName === message.message.upsname) {
             updateNut = true;
             adapter.setState('status.last_notify', {ack: true, val: message.message.notifytype});
-            if (message.message.notifytype==='COMMBAD' || message.message.notifytype==='NOCOMM') parseAndSetSeverity("OFF");
+            if (message.message.notifytype==='COMMBAD' || message.message.notifytype==='NOCOMM') {
+                parseAndSetSeverity("OFF");
+            }
         }
     }
     else updateNut = true;
@@ -286,25 +296,26 @@ function initNutConnection(callback) {
         return;
     }
 
-    var oNut = new Nut(adapter.config.host_port, adapter.config.host_ip);
+    const oNut = new Nut(adapter.config.host_port, adapter.config.host_ip);
 
-    oNut.on('error', function(err) {
+    oNut.on('error', (err) => {
         if (stopInProgress) return; // adapter already unloaded
-        adapter.log.error('Error happend: ' + err);
+        adapter.log.error('Error happened: ' + err);
         setConnected(false);
-        adapter.getState('status.last_notify', function (err, state) {
+        adapter.getState('status.last_notify', (err, state) => {
             if (!err && !state || (state && state.val!=='COMMBAD' && state.val!=='SHUTDOWN' && state.val!=='NOCOMM')) {
                 adapter.setState('status.last_notify', {ack: true, val: 'ERROR'});
             }
             if (!err) parseAndSetSeverity("");
+            callback(null);
         });
     });
 
-    oNut.on('close', function() {
+    oNut.on('close', () => {
         adapter.log.debug('NUT Connection closed. Done.');
     });
 
-    oNut.on('ready', function() {
+    oNut.on('ready', () => {
         adapter.log.debug('NUT Connection ready');
         setConnected(true);
         callback(oNut);
@@ -316,16 +327,16 @@ function initNutConnection(callback) {
 function updateNutData() {
     adapter.log.debug('Start NUT update');
 
-    initNutConnection(function(oNut) {
-        getCurrentNutValues(oNut, true);
+    initNutConnection((oNut) => {
+        oNut && getCurrentNutValues(oNut, true);
     });
 
-    var update_interval = parseInt(adapter.config.update_interval,10) || 60;
+    const update_interval = parseInt(adapter.config.update_interval, 10) || 60;
     nutTimeout = setTimeout(updateNutData, update_interval * 1000);
 }
 
 function getCurrentNutValues(oNut, closeConnection) {
-    oNut.GetUPSVars(adapter.config.ups_name, function(varlist, err) {
+    oNut.GetUPSVars(adapter.config.ups_name, (varlist, err) => {
         if (err) {
             adapter.log.error('Err while getting NUT values: '+ err);
         }
@@ -338,36 +349,38 @@ function getCurrentNutValues(oNut, closeConnection) {
 }
 
 async function storeNutData(varlist) {
-    var last='';
-    var current='';
-    var index=0;
-    var stateName='';
+    let last = '';
+    let current = '';
+    let index = 0;
+    let stateName = '';
 
-    for (var key in varlist) {
+    for (let key in varlist) {
         if (!varlist.hasOwnProperty(key)) continue;
 
-        index=key.indexOf('.');
+        index = key.indexOf('.');
         if (index > 0) {
-            current=key.substring(0,index);
+            current = key.substring(0, index);
         }
         else {
-            current='';
-            last='';
-            index=-1;
+            current = '';
+            last = '';
+            index = -1;
         }
-        if (((last==='') || (last!==current)) && (current!=='')) {
-            adapter.log.debug('Create Channel '+current);
+        if (((last === '') || (last !== current)) && (current !== '')) {
+            adapter.log.debug('Create Channel ' + current);
             try {
                 await adapter.setObjectNotExistsAsync(current, {
                     type: 'channel',
-                    common: {name: current},
+                    common: {
+                        name: current
+                    },
                     native: {}
                 });
             } catch (err) {
                 adapter.log.error(`Error creating Channel: ${err}`);
             }
         }
-        stateName=current+'.'+key.substring(index+1).replace(/\./g,'-');
+        stateName = current + '.' + key.substring(index + 1).replace(/\./g,'-');
         adapter.log.debug(`Create State ${stateName}`);
         if (stateName === 'battery.charge') {
             try {
@@ -395,50 +408,52 @@ async function storeNutData(varlist) {
         }
         adapter.log.debug(`Set State ${stateName} = ${varlist[key]}`);
         await adapter.setStateAsync(stateName, {ack: true, val: varlist[key]});
-        last=current;
+        last = current;
     }
 
     if (varlist['ups.status']) {
-        parseAndSetSeverity(varlist['ups.status']);
+        await parseAndSetSeverity(varlist['ups.status']);
     }
-    else parseAndSetSeverity("");
+    else {
+        await parseAndSetSeverity("");
+    }
 
     adapter.log.debug('All Nut values set');
 }
 
 async function parseAndSetSeverity(ups_status, createObjects) {
-    var statusMap = {
-              'OL':{name:'online',severity:'idle'},
-              'OB':{name:'onbattery',severity:'operating'},
-              'LB':{name:'lowbattery',severity:'operating_critical'},
-              'HB':{name:'highbattery',severity:'operating_critical'},
-              'RB':{name:'replacebattery',severity:'action_needed'},
-              'CHRG':{name:'charging',severity:'idle'},
-              'DISCHRG':{name:'discharging',severity:'operating'},
-              'BYPASS':{name:'bypass',severity:'action_needed'},
-              'CAL':{name:'calibration',severity:'operating'},
-              'OFF':{name:'offline',severity:'action_needed'},
-              'OVER':{name:'overload',severity:'action_needed'},
-              'TRIM':{name:'trimming',severity:'operating'},
-              'BOOST':{name:'boosting',severity:'operating'},
-              'FSD':{name:'shutdown',severity:'operating_critical'}
-            };
-    var severity = {
-              'idle':false,
-              'operating':false,
-              'operating_critical':false,
-              'action_needed':false
-            };
+    const statusMap = {
+        'OL': {name: 'online', severity: 'idle'},
+        'OB': {name: 'onbattery', severity: 'operating'},
+        'LB': {name: 'lowbattery', severity: 'operating_critical'},
+        'HB': {name: 'highbattery', severity: 'operating_critical'},
+        'RB': {name: 'replacebattery', severity: 'action_needed'},
+        'CHRG': {name: 'charging', severity: 'idle'},
+        'DISCHRG': {name: 'discharging', severity: 'operating'},
+        'BYPASS': {name: 'bypass', severity: 'action_needed'},
+        'CAL': {name: 'calibration', severity: 'operating'},
+        'OFF': {name: 'offline', severity: 'action_needed'},
+        'OVER': {name: 'overload', severity: 'action_needed'},
+        'TRIM': {name: 'trimming', severity: 'operating'},
+        'BOOST': {name: 'boosting', severity: 'operating'},
+        'FSD': {name: 'shutdown', severity: 'operating_critical'}
+    };
+    const severity = {
+        'idle': false,
+        'operating': false,
+        'operating_critical': false,
+        'action_needed': false
+    };
     if (ups_status.indexOf('FSD') !== -1) {
         ups_status += ' OB LB';
     }
-    var checker=' '+ups_status+' ';
-    var stateName="";
+    const checker = ' ' + ups_status + ' ';
+    let stateName = "";
     for (var idx in statusMap) {
         if (statusMap.hasOwnProperty(idx)) {
-            var found=(checker.indexOf(' ' + idx)>-1);
-            stateName='status.'+statusMap[idx].name;
-            adapter.log.debug('Create State '+stateName);
+            const found = (checker.indexOf(' ' + idx) > -1);
+            stateName = 'status.' + statusMap[idx].name;
+            adapter.log.debug('Create State ' + stateName);
             try {
                 createObjects && await adapter.setObjectNotExistsAsync(stateName, {
                     type: 'state',
@@ -451,18 +466,18 @@ async function parseAndSetSeverity(ups_status, createObjects) {
             adapter.log.debug('Set State '+stateName+' = '+found);
             await adapter.setStateAsync(stateName, {ack: true, val: found});
             if (found) {
-                severity[statusMap[idx].severity]=true;
-                adapter.log.debug('Severity Flag '+statusMap[idx].severity+'=true');
+                severity[statusMap[idx].severity] = true;
+                adapter.log.debug('Severity Flag ' + statusMap[idx].severity + '=true');
             }
         }
     }
-    var severityVal = 4;
-    if (severity.operating_critical) severityVal=2;
-        else if (severity.action_needed) severityVal=3;
-        else if (severity.operating) severityVal=1;
-        else if (severity.idle) severityVal=0;
+    let severityVal = 4;
+    if (severity.operating_critical) severityVal = 2;
+        else if (severity.action_needed) severityVal = 3;
+        else if (severity.operating) severityVal = 1;
+        else if (severity.idle) severityVal = 0;
 
-    adapter.log.debug('Set State status.severity = '+severityVal);
+    adapter.log.debug('Set State status.severity = ' + severityVal);
     await adapter.setStateAsync('status.severity', {ack: true, val: severityVal});
 }
 
